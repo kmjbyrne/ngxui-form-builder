ac8a5879d963a3b941c9729cbdae90fc23cf1e30
Keith Byrne <kmjbyrne@amazon.com>
2019-09-12 02:04:40 +0100

    updated public api to allow for FieldConfig to be consumed

--- projects/form-builder/src/public-api.ts
+++ projects/form-builder/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of form-builder
  */
 
+export { FieldConfig } from './lib/models/field-config.interface';
 export { NgxFormBuilderComponent } from './lib/ngx-form-builder.component';
 export { NgxFormBuilderModule } from './lib/ngx-form-builder.module';
